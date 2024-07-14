81461837d3789f0540177bdfd2fd51f541556f32
mosrafaOthman12 <mostafayoussef82@gmail.com>
2024-07-14 03:56:24 +0300

    implementing search.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from "@angular/common";
 import { HousingLoacationComponent } from "../housing-loacation/housing-loacation.component";
 import type { HousingLocation } from "../housing-location";
 import { HousingService } from "../housing.service";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-home",
@@ -12,13 +13,24 @@ import { HousingService } from "../housing.service";
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  housingLocations: HousingLocation[] | undefined = [];
+  housingLocations: HousingLocation[] = [];
   housingLocationService = inject(HousingService);
+  filteredList: HousingLocation[] = [];
+
+  onSearch(filter: string) {
+    if (!filter) this.filteredList = this.housingLocations;
+    else {
+      this.filteredList = this.housingLocations.filter(
+        (location) => location?.city.toLowerCase() === filter.toLowerCase()
+      );
+    }
+  }
 
   ngOnInit() {
     this.housingLocationService.getAllHousing().subscribe({
       next: (locations) => {
         this.housingLocations = locations;
+        this.filteredList = locations;
       },
       error: (error) => {
         console.error("Error fetching housing locations:", error);
